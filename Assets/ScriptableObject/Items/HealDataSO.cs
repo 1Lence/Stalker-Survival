3845dc647f62179bec4214e66b3ae179d60fcf01
Stealthmefie <stealthmefi@gmail.com>
2025-11-18 19:41:22 +0500

    Хилки

---
+++ Assets/ScriptableObject/Items/HealDataSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HealDataSO", menuName = "Scriptable Objects/HealDataSO")]
+public class HealDataSO : ScriptableObject
+{
+    public int healAmount = 20;
+}
